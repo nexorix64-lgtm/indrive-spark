ae5ccd935389951cb5ed6d6617638b89e5ab6830
nexorix64-lgtm <nexorix64@gmail.com>
2026-08-04 13:12:31 -0600

    Actualizar InDrive Spark a V3
    Actualizar InDrive Spark a V3

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='indrive-spark-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./historial.html','./ajustes.html','./style.css','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
